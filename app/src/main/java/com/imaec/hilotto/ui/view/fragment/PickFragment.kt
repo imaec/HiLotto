bc5254ed8a159762675a3f62ea9799106e2325f7
imaec <dev.imaec@gmail.com>
2020-07-13 19:03:20 +0900

    - 통계 기능 : 출현 번호 통계 기능 구현

--- app/src/main/java/com/imaec/hilotto/ui/view/fragment/PickFragment.kt
+++ app/src/main/java/com/imaec/hilotto/ui/view/fragment/PickFragment.kt
@@ -2,23 +2,40 @@ package com.imaec.hilotto.ui.view.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import com.imaec.hilotto.R
 import com.imaec.hilotto.base.BaseFragment
 import com.imaec.hilotto.databinding.FragmentPickBinding
+import com.imaec.hilotto.repository.FireStoreRepository
+import com.imaec.hilotto.repository.LottoRepository
+import com.imaec.hilotto.viewmodel.LottoViewModel
 import com.imaec.hilotto.viewmodel.PickViewModel
 
 class PickFragment : BaseFragment<FragmentPickBinding>(R.layout.fragment_pick) {
 
-    private lateinit var viewModel: PickViewModel
+    private lateinit var pickViewModel: PickViewModel
+    private lateinit var sharedViewModel: LottoViewModel
+
+    private val lottoRepository = LottoRepository()
+    private val firestoreRepository = FireStoreRepository()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = getViewModel(PickViewModel::class.java)
+        pickViewModel = getViewModel(PickViewModel::class.java)
+        sharedViewModel = getViewModel(LottoViewModel::class.java, activity!!, lottoRepository, firestoreRepository)
 
         binding.apply {
             lifecycleOwner = this@PickFragment
-            viewModel = this@PickFragment.viewModel
+            pickViewModel = this@PickFragment.pickViewModel
+            sharedViewModel = this@PickFragment.sharedViewModel
         }
+
+        sharedViewModel.listResult.observe(activity!!, Observer {
+            pickViewModel.setPickedNum(it)
+            binding.checkIncludeBonus.setOnCheckedChangeListener { compoundButton, b ->
+                pickViewModel.setPickedNum(it, b)
+            }
+        })
     }
 }
--- app/src/main/java/com/imaec/hilotto/viewmodel/PickViewModel.kt
+++ app/src/main/java/com/imaec/hilotto/viewmodel/PickViewModel.kt
@@ -1,6 +1,91 @@
 package com.imaec.hilotto.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.imaec.hilotto.base.BaseViewModel
+import com.imaec.hilotto.model.LottoDTO
 
 class PickViewModel : BaseViewModel() {
+
+    private val _listPick = MutableLiveData<List<LottoDTO>>().set(ArrayList())
+    val listPick: LiveData<List<LottoDTO>> get() = _listPick
+
+    private val _pickedRange1 = MutableLiveData<Int>().set(0)
+    val pickedRange1: LiveData<Int> get() = _pickedRange1
+
+    private val _pickedRange2 = MutableLiveData<Int>().set(0)
+    val pickedRange2: LiveData<Int> get() = _pickedRange2
+
+    private val _pickedRange3 = MutableLiveData<Int>().set(0)
+    val pickedRange3: LiveData<Int> get() = _pickedRange3
+
+    private val _pickedRange4 = MutableLiveData<Int>().set(0)
+    val pickedRange4: LiveData<Int> get() = _pickedRange4
+
+    private val _pickedRange5 = MutableLiveData<Int>().set(0)
+    val pickedRange5: LiveData<Int> get() = _pickedRange5
+
+    private val _pickMax = MutableLiveData<Int>().set(0)
+    val pickMax: LiveData<Int> get() = _pickMax
+
+    private fun initRange() {
+        _pickedRange1.value = 0
+        _pickedRange2.value = 0
+        _pickedRange3.value = 0
+        _pickedRange4.value = 0
+        _pickedRange5.value = 0
+    }
+
+    private fun setRange(number: Int) {
+        when (number) {
+            in 1..10 -> {
+                _pickedRange1.value?.let {
+                    _pickedRange1.value = it + 1
+                }
+            }
+            in 11..20 -> {
+                _pickedRange2.value?.let {
+                    _pickedRange2.value = it + 1
+                }
+            }
+            in 21..30 -> {
+                _pickedRange3.value?.let {
+                    _pickedRange3.value = it + 1
+                }
+            }
+            in 31..40 -> {
+                _pickedRange4.value?.let {
+                    _pickedRange4.value = it + 1
+                }
+            }
+            in 41..50 -> {
+                _pickedRange5.value?.let {
+                    _pickedRange5.value = it + 1
+                }
+            }
+        }
+    }
+
+    private fun getPickMax() : Int {
+        val listTemp = arrayListOf(_pickedRange1.value!!, _pickedRange2.value!!, _pickedRange3.value!!, _pickedRange4.value!!, _pickedRange5.value!!).sortedDescending()
+        listTemp.forEach {
+            Log.d(TAG, "    ## $it")
+        }
+        return listTemp[0]
+    }
+
+    fun setPickedNum(listResult: List<LottoDTO>, isInclude: Boolean = false) {
+        initRange()
+        listResult.subList(listResult.size - 21, listResult.size - 1).reversed().forEach {
+            setRange(it.drwtNo1)
+            setRange(it.drwtNo2)
+            setRange(it.drwtNo3)
+            setRange(it.drwtNo4)
+            setRange(it.drwtNo5)
+            setRange(it.drwtNo6)
+            if (isInclude) setRange(it.bnusNo)
+        }
+        _pickMax.value = getPickMax()
+    }
 }
