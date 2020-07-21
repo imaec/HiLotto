0bb11f70733b7672262b9e70cbccfdca15402250
imaec <dev.imaec@gmail.com>
2020-07-21 22:00:19 +0900

    - 통계 기능 : 당첨 통계 기능 구현 시작

--- app/src/main/java/com/imaec/hilotto/ui/view/fragment/WinFragment.kt
+++ app/src/main/java/com/imaec/hilotto/ui/view/fragment/WinFragment.kt
@@ -2,25 +2,32 @@ package com.imaec.hilotto.ui.view.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import com.imaec.hilotto.R
 import com.imaec.hilotto.base.BaseFragment
-import com.imaec.hilotto.databinding.FragmentSumBinding
 import com.imaec.hilotto.databinding.FragmentWinBinding
-import com.imaec.hilotto.viewmodel.SumViewModel
+import com.imaec.hilotto.viewmodel.LottoViewModel
 import com.imaec.hilotto.viewmodel.WinViewModel
 
 class WinFragment : BaseFragment<FragmentWinBinding>(R.layout.fragment_win) {
 
-    private lateinit var viewModel: WinViewModel
+    private lateinit var winViewModel: WinViewModel
+    private lateinit var sharedViewModel: LottoViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = getViewModel(WinViewModel::class.java)
+        winViewModel = getViewModel(WinViewModel::class.java)
+        sharedViewModel = getViewModel(LottoViewModel::class.java)
 
         binding.apply {
             lifecycleOwner = this@WinFragment
-            viewModel = this@WinFragment.viewModel
+            winViewModel = this@WinFragment.winViewModel
+            sharedViewModel = this@WinFragment.sharedViewModel
         }
+
+        sharedViewModel.listResult.observe(activity!!, Observer {
+            // winViewModel.setOddEven(it)
+        })
     }
 }
