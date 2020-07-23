a640b66247621a1d8076a71a2538e65442cacf11
imaec <dev.imaec@gmail.com>
2020-07-23 19:31:08 +0900

    - 통계 기능 : 당첨 통계 기능 - 1등 당첨 평균 구현

--- app/src/main/java/com/imaec/hilotto/ui/view/fragment/WinFragment.kt
+++ app/src/main/java/com/imaec/hilotto/ui/view/fragment/WinFragment.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.Observer
 import com.imaec.hilotto.R
 import com.imaec.hilotto.base.BaseFragment
 import com.imaec.hilotto.databinding.FragmentWinBinding
+import com.imaec.hilotto.repository.FireStoreRepository
+import com.imaec.hilotto.repository.LottoRepository
 import com.imaec.hilotto.viewmodel.LottoViewModel
 import com.imaec.hilotto.viewmodel.WinViewModel
 
@@ -14,11 +16,14 @@ class WinFragment : BaseFragment<FragmentWinBinding>(R.layout.fragment_win) {
     private lateinit var winViewModel: WinViewModel
     private lateinit var sharedViewModel: LottoViewModel
 
+    private val lottoRepository = LottoRepository()
+    private val firestoreRepository = FireStoreRepository()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         winViewModel = getViewModel(WinViewModel::class.java)
-        sharedViewModel = getViewModel(LottoViewModel::class.java)
+        sharedViewModel = getViewModel(LottoViewModel::class.java, activity!!, lottoRepository, firestoreRepository)
 
         binding.apply {
             lifecycleOwner = this@WinFragment
--- app/src/main/java/com/imaec/hilotto/viewmodel/WinViewModel.kt
+++ app/src/main/java/com/imaec/hilotto/viewmodel/WinViewModel.kt
@@ -1,11 +1,53 @@
 package com.imaec.hilotto.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.imaec.hilotto.base.BaseViewModel
 import com.imaec.hilotto.model.LottoDTO
 
 class WinViewModel : BaseViewModel() {
 
+    private val _price = MutableLiveData<Long>().set(0)
+    val price: LiveData<Long> get() = _price
+
+    private val _priceTotal = MutableLiveData<Long>().set(0)
+    val priceTotal: LiveData<Long> get() = _priceTotal
+
+    private val _winCount = MutableLiveData<String>().set("0명")
+    val winCount: LiveData<String> get() = _winCount
+
+    private fun getPriceAvg(list: List<LottoDTO>): Long {
+        var sum: Long = 0
+        list.forEach {
+            sum += it.firstWinamnt
+        }
+        return sum / list.size
+    }
+
+    private fun getPriceTotalAvg(list: List<LottoDTO>): Long {
+        var sum: Long = 0
+        list.forEach {
+            sum += it.firstAccumamnt
+        }
+        return sum / list.size
+    }
+
+    private fun getWinCount(list: List<LottoDTO>): String {
+        var sum = 0
+        list.forEach {
+            sum += it.firstPrzwnerCo
+        }
+        return "${sum / list.size}명"
+    }
+
     fun setWinInfo(listResult: List<LottoDTO>) {
-        
+        val listTemp = ArrayList<LottoDTO>()
+        listResult.subList(listResult.size - 21, listResult.size - 1).reversed().forEach {
+            listTemp.add(it)
+        }
+
+        _price.value = getPriceAvg(listTemp)
+        _priceTotal.value = getPriceTotalAvg(listTemp)
+        _winCount.value = getWinCount(listTemp)
     }
 }
