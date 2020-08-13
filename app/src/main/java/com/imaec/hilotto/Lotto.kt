902fd2511c3678f2e3ae9ba001d7b5b31699a5de
imaec <30709024+imaec@users.noreply.github.com>
2020-08-13 14:25:31 +0900

    - 추천 번호 생성 : 생성 번호 개수, 번호 합계 범위 수정 가능하도록 수정

--- app/src/main/java/com/imaec/hilotto/Lotto.kt
+++ app/src/main/java/com/imaec/hilotto/Lotto.kt
@@ -9,66 +9,50 @@ import kotlin.math.roundToInt
 
 object Lotto {
 
-    fun getSumAvg(list: ArrayList<LottoDTO>): Int {
-        var sum = 0
-        list.map {
-            it.drwtNo1 + it.drwtNo2 + it.drwtNo3 + it.drwtNo4 + it.drwtNo5 + it.drwtNo6
-        }.forEach {
-            sum += it
-        }
-        return sum / list.size
+    private var minRange = 25
+    private var maxRange = 25
+    private var count = 6
+
+    private fun getSumMin(list: ArrayList<LottoDTO>): Int {
+        return getSumAvg(list) - minRange
     }
 
-    fun getSumMin(list: ArrayList<LottoDTO>): Int {
-        return getSumAvg(list) - 25
+    private fun getSumMax(list: ArrayList<LottoDTO>): Int {
+        return getSumAvg(list) + maxRange
     }
 
-    fun getSumMax(list: ArrayList<LottoDTO>): Int {
-        return getSumAvg(list) + 25
-    }
-
-    fun getNumbers(list: ArrayList<LottoDTO>): ArrayList<Int> {
-        var result = ArrayList<Int>()
-        val sumMin = getSumMin(list)
-        val sumMax = getSumMax(list)
-        var isContinue = true
-
-        while (isContinue) {
-            var sum = 0
-            result = getRanNumbers(list)
-            result.forEach {
-                sum += it
-            }
-//            println(sum)
-
-            result.sort()
-//            println(result)
-            if (sum in sumMin..sumMax) {
-                isContinue = false
+    private fun getWeight(list: ArrayList<LottoDTO>): HashMap<Int, Int> {
+        val w = HashMap<Int, Int>()
+        val countSum = ArrayList<Int>().apply {
+            (0..44).forEach { _ ->
+                add(0)
             }
         }
 
-        return result
-    }
-
-    private fun getRanNumbers(list: ArrayList<LottoDTO>): ArrayList<Int> {
-        val result = arrayListOf<Int>()
-        var isContinue = true
-        while (isContinue) {
-            var isExist = false
-            val number = getWeightedRandom(list)
-
-            result.forEach {
-                if (it == number) {
-                    isExist = true
-                    return@forEach
-                }
-            }
-            if (!isExist) result.add(number)
-            if (result.size == 6) isContinue = false
+        list.forEach {
+            countSum[it.drwtNo1 - 1] += 1
+            countSum[it.drwtNo2 - 1] += 1
+            countSum[it.drwtNo3 - 1] += 1
+            countSum[it.drwtNo4 - 1] += 1
+            countSum[it.drwtNo5 - 1] += 1
+            countSum[it.drwtNo6 - 1] += 1
         }
 
-        return result
+        var bestValue = 0
+        countSum.forEachIndexed { i, count ->
+            val weightTemp = (if (count == 0) 1.0 else 1.0/count)
+            val weight = (weightTemp * 100).roundToInt()
+            if (weight in bestValue..99) {
+                bestValue = weight
+            }
+            w[i] = weight
+        }
+        for (key in w.keys) {
+            if (w[key]!! == 100) {
+                w[key] = bestValue + 10
+            }
+        }
+        return w
     }
 
     private fun getWeightedRandom(list: ArrayList<LottoDTO>): Int {
@@ -107,39 +91,85 @@ object Lotto {
         return selection
     }
 
-    fun getWeight(list: ArrayList<LottoDTO>): HashMap<Int, Int> {
-        val w = HashMap<Int, Int>()
-        val countSum = ArrayList<Int>().apply {
-            (0..44).forEach { _ ->
-                add(0)
+    private fun getRanNumbers(list: ArrayList<LottoDTO>): ArrayList<Int> {
+        val result = arrayListOf<Int>()
+        var isContinue = true
+        while (isContinue) {
+            var isExist = false
+            val number = getWeightedRandom(list)
+
+            result.forEach {
+                if (it == number) {
+                    isExist = true
+                    return@forEach
+                }
+            }
+            if (!isExist) result.add(number)
+            if (result.size == count) isContinue = false
+        }
+
+        return result
+    }
+
+    private fun getSumAvg(list: ArrayList<LottoDTO>): Int {
+        var sum = 0
+        list.map {
+            it.drwtNo1 + it.drwtNo2 + it.drwtNo3 + it.drwtNo4 + it.drwtNo5 + it.drwtNo6
+        }.forEach {
+            sum += it
+        }
+        return sum / list.size
+    }
+
+    private fun getOdd(vararg numbers: Int): List<String> {
+        val listOdd = ArrayList<String>()
+        numbers.forEach { number ->
+            if (number%2 != 0) listOdd.add("$number")
+        }
+        return listOdd
+    }
+
+    private fun getEven(vararg numbers: Int): List<String> {
+        val listEven = ArrayList<String>()
+        numbers.forEach { number ->
+            if (number%2 != 0) listEven.add("$number")
+        }
+        return listEven
+    }
+
+    fun getNumbers(list: ArrayList<LottoDTO>): ArrayList<Int> {
+        var result = ArrayList<Int>()
+        val sumMin = getSumMin(list)
+        val sumMax = getSumMax(list)
+        var isContinue = true
+
+        while (isContinue) {
+            var sum = 0
+            result = getRanNumbers(list)
+            // 생성된 번호의 합이 지정된 범위안에 들어오는지 확인
+            result.forEach {
+                sum += it
+            }
+            if (sum in sumMin..sumMax &&
+                getOdd(result[0], result[1], result[2], result[3], result[4], result[5]).size in 2..4 &&
+                getEven(result[0], result[1], result[2], result[3], result[4], result[5]).size in 2..4) {
+                isContinue = false
             }
         }
 
-        list.forEach {
-            countSum[it.drwtNo1 - 1] += 1
-            countSum[it.drwtNo2 - 1] += 1
-            countSum[it.drwtNo3 - 1] += 1
-            countSum[it.drwtNo4 - 1] += 1
-            countSum[it.drwtNo5 - 1] += 1
-            countSum[it.drwtNo6 - 1] += 1
-        }
+        result.sort()
+        return result
+    }
 
-        var bestValue = 0
-        countSum.forEachIndexed { i, count ->
-            val weightTemp = (if (count == 0) 1.0 else 1.0/count)
-//            weight = (weight * 100).roundToInt() / 100.0
-            val weight = (weightTemp * 100).roundToInt()
-            if (weight in bestValue..99) {
-                bestValue = weight
-            }
-            w[i] = weight
-            // println("${i+1} : $count / $weight")
-        }
-        for (key in w.keys) {
-            if (w[key]!! == 100) {
-                w[key] = bestValue + 10
-            }
-        }
-        return w
+    fun setMinRange(min: Int) {
+        this.minRange = min
+    }
+
+    fun setMaxRange(max: Int) {
+        this.maxRange = max
+    }
+
+    fun setCount(count: Int) {
+        this.count = count
     }
 }
