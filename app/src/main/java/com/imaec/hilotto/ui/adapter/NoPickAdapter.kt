28f587dc0026eb72d30a3a73a37b1a00178134f0
imaec <dev.imaec@gmail.com>
2020-08-31 16:17:51 +0900

    - 합계 통계 그레프에서 최고값과 최저값 색상 적용

--- app/src/main/java/com/imaec/hilotto/ui/adapter/NoPickAdapter.kt
+++ app/src/main/java/com/imaec/hilotto/ui/adapter/NoPickAdapter.kt
@@ -24,6 +24,7 @@ class NoPickAdapter : BaseAdapter() {
     inner class ItemViewHolder(val binding: ItemNopickBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun onBind(item: Int) {
+            
             binding.apply {
                 this.item = item
             }
