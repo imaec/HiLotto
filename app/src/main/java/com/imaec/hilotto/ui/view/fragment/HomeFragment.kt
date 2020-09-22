3b791ebf8cf4caee4fde5e650516a6bd6b16a726
imaec <dev.imaec@gmail.com>
2020-09-22 18:23:29 +0900

    - 공유하기 기능 추가

--- app/src/main/java/com/imaec/hilotto/ui/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/imaec/hilotto/ui/view/fragment/HomeFragment.kt
@@ -2,10 +2,13 @@ package com.imaec.hilotto.ui.view.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.ads.AdRequest
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.imaec.hilotto.*
 import com.imaec.hilotto.base.BaseFragment
 import com.imaec.hilotto.databinding.FragmentHomeBinding
@@ -19,6 +22,10 @@ import com.imaec.hilotto.ui.view.activity.StoreActivity
 import com.imaec.hilotto.ui.view.activity.WinHistoryActivity
 import com.imaec.hilotto.viewmodel.HomeViewModel
 import com.imaec.hilotto.viewmodel.LottoViewModel
+import com.kakao.kakaolink.v2.KakaoLinkResponse
+import com.kakao.kakaolink.v2.KakaoLinkService
+import com.kakao.network.ErrorResult
+import com.kakao.network.callback.ResponseCallback
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
@@ -72,12 +79,26 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                 }
             }
             R.id.text_store -> {
-                sharedViewModel.listStore.value?.let {
-                    startActivity(Intent(context, StoreActivity::class.java).apply {
-                        putExtra(EXTRA_CURRENT_ROUND, sharedViewModel.curDrwNo.value ?: 0)
-                        putExtra(EXTRA_LIST_STORE, it as ArrayList<StoreDTO>)
+                KakaoLinkService.getInstance()
+                    .sendCustom(context, getString(R.string.template_id_app), null, object : ResponseCallback<KakaoLinkResponse>() {
+                        override fun onSuccess(result: KakaoLinkResponse?) {
+//                            logEvent(FirebaseAnalytics.Event.SHARE, Bundle().apply {
+//                                putString(FirebaseAnalytics.Param.ITEM_CATEGORY, viewModel.category.value)
+//                                putString(FirebaseAnalytics.Param.ITEM_NAME, viewModel.title.value)
+//                            })
+                        }
+
+                        override fun onFailure(errorResult: ErrorResult?) {
+                            Log.e(TAG, "    ## ${errorResult.toString()}")
+                            Toast.makeText(context, R.string.msg_unknown_error, Toast.LENGTH_SHORT).show()
+                        }
                     })
-                }
+//                sharedViewModel.listStore.value?.let {
+//                    startActivity(Intent(context, StoreActivity::class.java).apply {
+//                        putExtra(EXTRA_CURRENT_ROUND, sharedViewModel.curDrwNo.value ?: 0)
+//                        putExtra(EXTRA_LIST_STORE, it as ArrayList<StoreDTO>)
+//                    })
+//                }
             }
         }
     }
